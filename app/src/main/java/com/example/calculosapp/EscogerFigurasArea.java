930c54ef80dab548f62e56306e400148d7d36421
Daniela Toncel Molina <daniela.toncel.ext@kavak.com>
2020-11-20 16:05:50 -0500

    Layouts con ListViews de opciones, áreas y volúmenes + cascarones de calcular áreas y volúmenes

---
+++ app/src/main/java/com/example/calculosapp/EscogerFigurasArea.java
@@ -0,0 +1,41 @@
+package com.example.calculosapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class EscogerFigurasArea extends AppCompatActivity {
+    private ListView lista_figuras_area;
+    private String figuras_area[];
+    private ArrayAdapter<String> adapter;
+    private Intent intent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_escoger_figuras_area);
+
+        lista_figuras_area =findViewById(R.id.lstFigurasArea);
+        figuras_area = getResources().getStringArray(R.array.figuras_area);
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, figuras_area);
+        lista_figuras_area.setAdapter(adapter);
+
+        lista_figuras_area.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (position){
+                    case 0:
+                        intent = new Intent(EscogerFigurasArea.this, CalcularAreas.class);
+                        startActivity(intent);
+                        break;
+                }
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/calculosapp/EscogerFigurasVolumen.java
@@ -0,0 +1,40 @@
+package com.example.calculosapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class EscogerFigurasVolumen extends AppCompatActivity {
+    private ListView lista_figura_volumen;
+    private String figuras_volumen [];
+    private ArrayAdapter<String> adapter;
+    private Intent intent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_escoger_figuras_volumen);
+
+        lista_figura_volumen = findViewById(R.id.lstFigurasVolumen);
+        figuras_volumen = getResources().getStringArray(R.array.figuras_volumen);
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, figuras_volumen);
+        lista_figura_volumen.setAdapter(adapter);
+
+        lista_figura_volumen.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (position){
+                    case 0:
+                        intent = new Intent(EscogerFigurasVolumen.this, CalcularVolumenes.class);
+                        startActivity(intent);
+                        break;
+                }
+            }
+        });
+    }
+}
